f890b78fab33fecc2d6e5162a17c03fbcc5347cc
hey_anthony <tonymalakhov@gmail.com>
2018-05-14 16:19:44 +0300

    initial commit

---
+++ main.py
@@ -0,0 +1,9 @@
+print('Это мой первый репозиторий git')
+answer = input('Ты рад?\n')
+
+answerS = {'да', 'конечно', 'ок', 'очень рад', 'супер'}
+
+if answer in answerS:
+    print('Отлично')
+else:
+    print('Ну и ладно')
