64d088c5263ad15b8a932db3dd400c35464bbb94
hey_anthony <tonymalakhov@gmail.com>
2018-05-21 13:02:53 +0300

    The work of the note has been reworked, a special class for work has been made

--- main.py
+++ main.py
@@ -1,40 +1,8 @@
-# тестовое приложение
+# тестовое приложение записной книжки
+from rwnote import *
 print('Эта простая записная книжка. \nВыберете !создать !редактировать !прочитать \n')
 
-answer = input()
-if answer == '!создать':
-    mode = 'w'
-    filename = input('Укажите название файла: \n')
-    # filename = 'file.txt'
-    print('Введите текст который нужно сохранить в файле. Чтобы закончить ввод введите !сохранить.\n Введите текст: \n')
-    text = ''
-    tmp = str(input())
-    while tmp != '!сохранить':
-        text = text + tmp + '\n'
-        tmp = str(input())
+answer = str(input())
+note = RWnote(answer)
+note.canNote()
 
-    f1 = open(filename, 'w', encoding='utf8')
-    f1.write(text)
-    f1.close()
-
-elif answer == '!прочитать':
-    mode = 'r'
-    filename = input('Укажите название файла: \n')
-    f1 = open(filename, 'r', encoding='utf8')
-    print(f1.read())
-    f1.close()
-elif answer == '!редактировать':
-    mode = 'a'
-    filename = input('Укажите название файла: \n')
-    # filename = 'file.txt'
-    print('Введите текст который нужно сохранить в файле. Чтобы закончить ввод введите !сохранить.\n Введите текст: \n')
-    tmp = str(input())
-    while tmp != '!сохранить':
-        f1 = open(filename, 'a', encoding='utf8')
-        f1.write(tmp + '\n')
-        f1.close()
-        tmp = str(input())
-else:
-    print('Ошибка! Неизвестная команда.')
-
-print('Завершение работы')
--- rwnote.py
+++ rwnote.py
@@ -1,13 +1,48 @@
 class RWnote():
-    """Класс работы с записями"""
+    """Класс работы с записями умеет r - читать, w - записывать(перезаписывать заново), добавлять в файл"""
 
-    def __init__(self, mode, name):
+    def __init__(self, mode):
         self.mode = mode
-        self.name = name
+        self.filename = input('Укажите название файла: \n')
     pass
 
     def readNote(self):
-        file = open(self.name, 'r', encoding='utf8')
-        file.read()
+        file = open(self.filename, 'r', encoding='utf8')
+        print(file.read())
         file.close()
         return
+
+    def writeNote(self):
+        text = ''
+        tmp = str(input())
+        while tmp != '!сохранить':
+            text = text + tmp + '\n'
+            tmp = str(input())
+        file = open(self.filename, 'w', encoding='utf8')
+        file.write(text)
+        file.close()
+        return
+
+    def addNote(self):
+        text = ''
+        tmp = str(input())
+        while tmp != '!сохранить':
+            file = open(self.filename, 'a', encoding='utf8')
+            file.write(tmp + '\n')
+            tmp = str(input())
+        return
+
+    def canNote(self):
+        # filename = input('Укажите название файла: \n')
+        print(self.filename + '    ' + self.mode)
+        if self.mode == '!прочитать':
+            self.readNote()
+        elif self.mode == '!создать':
+            self.writeNote()
+        elif self.mode == '!дописать':
+            self.addNote()
+        else:
+            print('Ошибка! Неизвестная команда.')
+        print('Завершение работы')
+        return
+
